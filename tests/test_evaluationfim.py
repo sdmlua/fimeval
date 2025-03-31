807ff83021cd0d8be19507c6525b3176c7b45d23
supathdhitalGEO <sdhital@crimson.ua.edu>
2025-03-31 00:52:52 -0500

    added automatic detection of shapefile from a case folder

--- tests/test_evaluationfim.py
+++ tests/test_evaluationfim.py
@@ -2,21 +2,23 @@ import fimpef as fp
 from pathlib import Path
 
 Main_dir = (
-    "../docs/sampledata"
+    # "../docs/sampledata"
+    '/Users/supath/Downloads/MSResearch/FIMpef/CodeUsage/SampleData/Data/Neuse'
 )
 PWD_dir = "./path/to/PWB"
-output_dir = "../docs/Output"
+# output_dir = "../docs/Output"
+output_dir = '/Users/supath/Downloads/MSResearch/FIMpef/CodeUsage/SampleData/Output'
 
 building_footprint = "./path/to/building_footprint"
 
 # If user is working with user defined shapefile
-AOI = "./path/to/shapefile"
+# AOI = "./path/to/shapefile"
 
 # Three methods are available to evaluate the FIM,
 # 1. Smallest extent
 # 2. Convex Hull
 # 3. AOI (User defined shapefile)
-method_name = "smallest_extent"
+method_name = "AOI"
 
 # 3 letter country ISO code
 countryISO = "USA"
@@ -30,15 +32,15 @@ fp.EvaluateFIM(Main_dir, method_name, output_dir)
 
 
 # Once the FIM evaluation is done, print the contingency map
-# fp.PrintContingencyMap(Main_dir, method_name, output_dir)
+fp.PrintContingencyMap(Main_dir, method_name, output_dir)
 
 
 # Plot rhe evaluation metrics after the FIM evaluation
-# fp.PlotEvaluationMetrics(Main_dir, method_name, output_dir)
+fp.PlotEvaluationMetrics(Main_dir, method_name, output_dir)
 # FIM Evaluation with Building Footprint (by default, it uses the Microsoft Building Footprint dataset)
-# fp.EvaluationWithBuildingFootprint(
-#     Main_dir, method_name, output_dir, country=countryISO
-# )
+fp.EvaluationWithBuildingFootprint(
+    Main_dir, method_name, output_dir, country=countryISO
+)
 
 # If user have their own building footprint dataset, they can use it as well
 # fp.EvaluationWithBuildingFootprint(Main_dir, method_name, output_dir, building_footprint = building_footprint, shapefile_dir=AOI)
